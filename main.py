976e99ec367d6f820f6645defca41c104ec9ce8c
Agchai52 <shuangzhang0809@gmail.com>
2021-04-16 16:53:40 -0400

    Fix: bug

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ parser.add_argument('--dataset_name', dest='dataset_name', default='DeblurMicroc
 parser.add_argument('--epoch', dest='epoch', type=int, default=10, help='# of training epochs')
 parser.add_argument('--epoch_start', dest='epoch_start', type=int, default=0, help='# of start epoch')
 parser.add_argument('--epoch_decay', dest='epoch_decay', type=int, default=0, help='# of epoch to linearly decay lr to 0')
-parser.add_argument('--batch_size', dest='batch_size', type=int, default=2, help='# images in batch')
+parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')
 parser.add_argument('--load_size', dest='load_size', type=int, default=256, help='scale images to this size')
 parser.add_argument('--fine_size', dest='fine_size', type=int, default=64, help='then crop to this size')
 parser.add_argument('--ngf', dest='ngf', type=int, default=64, help='# of gen filters in first conv layer')
